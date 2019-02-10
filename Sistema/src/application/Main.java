a5036059bdba67a4a5a824fc0aee29596b018139
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2019-02-10 19:57:18 -0200

    Adicionado a tela de login.
    Adicionado a tela de login no sistema.

---
+++ Sistema/src/application/Main.java
@@ -0,0 +1,47 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//Comentar esta linha para teste.
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/guiMain/Login.fxml"));
+			AnchorPane scPnTelaPrincipal = loader.load();
+			
+			//Usado para teste, setar o fxml da tela que está testando apra abrir ela ao invez da tela de usuario.
+		//FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/Main.fxml"));
+		//	AnchorPane scPnTelaPrincipal = loader.load();
+			
+			//FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/Entregas.fxml"));
+			//AnchorPane scPnTelaPrincipal = loader.load();
+
+			mainScene = new Scene(scPnTelaPrincipal); // Carrega a scena
+			mainScene.setFill(Color.TRANSPARENT);
+			
+			primaryStage.setScene(mainScene); // Seta a cena principal
+			primaryStage.setTitle("Shiyoken");
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("../images/login/Shiyoken.png")));
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.show(); // Mostra a tela.
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Sistema/src/guiMain/LoginController.java
@@ -0,0 +1,67 @@
+package guiMain;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+
+public class LoginController implements Initializable {
+	
+	public static String usuario;
+	public static Stage loginStage;
+	
+	@FXML
+	private PasswordField pswFieldPassword;
+	@FXML
+	private Button btnExit;
+	@FXML
+	private Button btnLogin;
+	
+	@FXML
+	private ComboBox<String> cbBoxUsuario;
+	private ObservableList<String> obsList;
+	
+	@FXML
+	public void onBtExitAction() {
+		
+	}
+	
+	@FXML
+	public void onDrLoginExited() {
+	
+	}
+	
+	@FXML
+	public void onPasswordEnter(KeyEvent e) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	
+	}
+	
+	@FXML
+	public void onUserEnter(KeyEvent e) throws NoSuchAlgorithmException, UnsupportedEncodingException{
+	
+	}
+	
+	@FXML
+	public void onBtLoginAction() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+
+
+	}
+	
+	public void initialize(URL url, ResourceBundle rb) {
+
+	}
+	
+	public void ValidaLogin() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		
+	
+	}
+}
