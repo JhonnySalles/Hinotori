9648b016aeade1638b48d47849188c6057729bac
Rafael-Yutaka <rafael.yutaka@hotmail.com>
2019-02-16 00:18:48 -0200

    Conexão com banco de dados
    Ajustado configuração do xml
    Criado classe de modelagem da tabela usuarios
    Criado classe para criação da tabela Usuarios no banco de dados
    Adicionado comando no main para chamar classe de criação de tabela
    Realocado pasta META-INF no SRC.

--- Sistema/src/application/Main.java
+++ Sistema/src/application/Main.java
@@ -8,6 +8,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import servidor.persistencia.Controlador;
 
 
 public class Main extends Application {
@@ -42,6 +43,7 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		Controlador com = new Controlador();
 		launch(args);
 	}
 }
