4c94a370bd69ff5a274b622cacd1af14f07556b3
Rafael-Yutaka <rafael.yutaka@hotmail.com>
2019-03-06 22:33:11 -0300

    Configurando hibernate

--- Hinotori/Servidor/src/main/java/Servidor/App.java
+++ Hinotori/Servidor/src/main/java/Servidor/App.java
@@ -1,5 +1,7 @@
 package Servidor;
 
+import Servidor.persistencia.Controlador;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       Controlador com = new Controlador();
     }
 }
