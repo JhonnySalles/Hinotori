cf9700d9a3a4ae925a7050226aa0cd13732f627c
JhonnySalles <jhonnysallesnoschag@hotmail.com>
2021-01-03 17:34:05 -0300

    #PDV - Configurado o projeto para poder abrir com o comando java -jar.

---
+++ Hinotori/PDV/src/main/java/pdv/Run.java
@@ -0,0 +1,85 @@
+package pdv;
+
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import cadastro.controller.cadastros.CadClienteController;
+import cadastro.controller.cadastros.CadEmpresaController;
+import cadastro.controller.cadastros.CadProdutoController;
+import cadastro.controller.cadastros.CadUsuarioController;
+import comum.model.utils.ViewGerenciador;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import pdv.controller.DashboardController;
+
+public class Run extends Application {
+
+	private final static Logger LOGGER = Logger.getLogger(Run.class.getName());
+
+	private static Scene mainScene;
+	private static DashboardController mainController;
+
+	@Override
+	public synchronized void start(Stage primaryStage) {
+		try {
+			// Classe inicial
+			FXMLLoader loader = new FXMLLoader(DashboardController.getFxmlLocate());
+			AnchorPane scPnTelaPrincipal = loader.load();
+			mainController = loader.getController();
+
+			mainScene = new Scene(scPnTelaPrincipal); // Carrega a scena
+			mainScene.setFill(Color.BLACK);
+
+			primaryStage.setScene(mainScene); // Seta a cena principal
+			primaryStage.setTitle("Ponto de venda");
+			primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/pdv/imagens/icon/icoPDV_400.png")));
+			primaryStage.setMinHeight(600);
+			primaryStage.setMinWidth(600);
+			primaryStage.initStyle(StageStyle.DECORATED);
+			primaryStage.centerOnScreen();
+			primaryStage.setMaximized(true);
+
+			primaryStage.show(); // Mostra a tela.
+
+			Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+			primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+			primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
+
+			preCarregamentoTelas();
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOGGER.log(Level.SEVERE, "{Erro ao iniciar o programa}", e);
+		}
+	}
+
+	private static void preCarregamentoTelas() {
+		URL[] listaTelas = { CadClienteController.getFxmlLocate(), CadEmpresaController.getFxmlLocate(),
+				CadProdutoController.getFxmlLocate(), CadUsuarioController.getFxmlLocate() };
+		ViewGerenciador.setCaminhoTelasPreCarregamento(listaTelas);
+
+		String[] css = { Run.class.getResource("/pdv/css/Paleta_Cores.css").toExternalForm(),
+				Run.class.getResource("/pdv/css/White_Dashboard.css").toExternalForm(),
+				Run.class.getResource("/pdv/css/White_Dashboard_Botoes.css").toExternalForm(),
+				Run.class.getResource("/pdv/css/White_Dashboard_Graficos.css").toExternalForm(),
+				Run.class.getResource("/pdv/css/White_Geral.css").toExternalForm(),
+				Run.class.getResource("/pdv/css/White_SpinnerHorizontal.css").toExternalForm() };
+		ViewGerenciador.carregaCss(css);
+	}
+
+	public static DashboardController getMainController() {
+		return mainController;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
